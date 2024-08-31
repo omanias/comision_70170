d9d4e2d5c0d6812c3b92733b6e9035a4a8114ef5
omarmaniaslh <omar@legalhub.la>
2024-08-31 12:31:08 -0300

    agrego websockets

---
+++ websockets/src/app.js
@@ -0,0 +1,32 @@
+import express, { json, urlencoded } from "express"
+import handlebars from 'express-handlebars'
+import __dirname from './utils.js'
+import viewsRouter from './routes/views.router.js'
+import { Server } from "socket.io"
+
+const app = express()
+const PORT = 8080
+
+app.use(json())
+app.use(urlencoded({ extended: true }))
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+app.use(express.static(__dirname + '/public'))
+
+app.use('/', viewsRouter)
+
+const httpServer = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+const socketServer = new Server(httpServer)
+
+socketServer.on('connection', socket => {
+    console.log("Nuevo cliente conectado")
+
+    socket.on('message', data => {
+        console.log(`Soy la data ${data}`)
+    })
+})
